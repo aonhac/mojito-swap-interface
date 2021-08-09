8ba447d15ac980cefdd7e98063f1ddff3d1a0ba0
RyuProject <yunocoik@yahoo.co.jp>
2021-08-09 18:06:04 +0800

    try fix menu color

--- src/uikit/widgets/Menu/components/MenuEntry.tsx
+++ src/uikit/widgets/Menu/components/MenuEntry.tsx
@@ -34,7 +34,8 @@ const MenuEntry = styled.div<Props>`
   padding: ${({ secondary }) => (secondary ? '0 32px' : '0 16px')};
   font-size: ${({ secondary }) => (secondary ? '14px' : '16px')};
   background-color: ${({ secondary, theme }) => (secondary ? theme.colors.background : 'transparent')};
-  color: ${({ theme }) => theme.colors.textSubtle};
+  // color: ${({ theme }) => theme.colors.textSubtle};
+  color:#color: #5DDA98;
   box-shadow: ${({ isActive, theme }) => (isActive ? `inset 4px 0px 0px ${theme.colors.primary}` : 'none')};
 
   a {
