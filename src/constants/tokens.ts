00a9d79c3751dcef29c13e852e0ed9d56a214102
bbelly <kcsfans@163.com>
2021-08-20 15:31:17 +0800

    feat: 🎸 update fee config

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -10,8 +10,8 @@ const tokens = {
   wkcs: {
     symbol: 'WKCS',
     address: {
-      321: '0x6551358EDC7fee9ADAB1E2E49560E68a12E82d9e',
-      322: '0x4446fc4eb47f2f6586f9faab68b3498f86c07521',
+      321: '0x4446fc4eb47f2f6586f9faab68b3498f86c07521',
+      322: '0x6551358EDC7fee9ADAB1E2E49560E68a12E82d9e',
     },
     decimals: 18,
     projectLink: '',
